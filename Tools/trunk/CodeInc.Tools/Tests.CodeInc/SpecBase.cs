4f060bf6b25748a7b584d5b87d44d743cfbefa7b
steveharman <steven@harmanly.com>
2008-01-26 20:18:38 +0000

    Adding the CodeInc.Tools solution

---
+++ Tools/trunk/CodeInc.Tools/Tests.CodeInc/SpecBase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using MbUnit.Framework;
+using Rhino.Mocks;
+
+namespace Tests.CodeInc
+{
+    [TestFixture]
+    public class SpecBase
+    {
+        private MockRepository _mocks;
+        public MockRepository Mocks
+        {
+            get { return _mocks; }
+        }
+
+        public IDisposable Record
+        {
+            get { return _mocks.Record(); }
+        }
+
+        public IDisposable Playback
+        {
+            get { return _mocks.Playback(); }
+        }
+
+        public T Create<T>()
+        {
+            return Mocks.CreateMock<T>();
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _mocks = new MockRepository();
+
+            Before_Each_Spec();
+        }
+
+        public virtual void Before_Each_Spec()
+        {
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            After_Each_Spec();
+        }
+
+        public virtual void After_Each_Spec()
+        {
+        }
+    }
+}
