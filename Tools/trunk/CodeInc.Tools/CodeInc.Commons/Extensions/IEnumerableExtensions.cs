21d2edb3afd1e0ca5d03ede33f757454a66223e0
steveharman <steveharman@b7d3163e-863a-0410-ab12-b38f340628c1>
2008-01-26 20:18:38 +0000

    Adding the CodeInc.Tools solution

---
+++ Tools/trunk/CodeInc.Tools/CodeInc.Commons/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeInc.Commons.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// Executes the <paramref name="action"/> for each item in the IEnumerable collection.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="items"></param>
+        /// <param name="action">The action (expression) to execute</param>
+        /// <example>
+        /// This behaves much like code blocks in Ruby, allowing us to do things like
+        ///
+        /// int[] list = {1,2,3,4,5,6,7,8,9,10};
+        /// var sum = 0;
+        /// list.Each(i => sum += i);
+        /// </example>
+        public static void Each<T>(this IEnumerable<T> items, Action<T> action)
+        {
+            if (items == null)
+                throw new ArgumentNullException("items");
+
+            foreach (var item in items)
+            {
+                action(item);
+            }
+        }
+    }
+}
---
+++ Tools/trunk/CodeInc.Tools/Tests.CodeInc/Commons/Extensions/IEnumerableSpec.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using CodeInc.Commons.Extensions;
+using MbUnit.Framework;
+using Rhino.Mocks;
+
+namespace Tests.CodeInc.Commons.Extensions
+{
+    public class When_Executing_Each_On_IEnumerable_That_Is_Null : SpecBase
+    {
+        [Test]
+        [ExpectedArgumentNullException]
+        public void Should_Throw_NullArgumentException()
+        {
+            IEnumerable<ITestWidget> widgets = null;
+
+            widgets.Each(w =>
+            {
+                w.Foo();
+            });
+        }
+    }
+
+    public class When_Executing_Each_On_IEnumerable_That_Has_Items : SpecBase
+    {
+        [Test]
+        public void Should_Executes_CodeBlock_On_Every_Item()
+        {
+            ITestWidget myWidget = Create<ITestWidget>();
+            IEnumerable<ITestWidget> widgets = new ITestWidget[] { myWidget, myWidget, myWidget, myWidget, myWidget };
+
+            using (Record)
+            {
+                Expect.Call(myWidget.Foo()).Return("I was called!").Repeat.Times(5);
+            }
+
+            using (Playback)
+            {
+                widgets.Each(w =>
+                {
+                    string output = w.Foo();
+                    Console.WriteLine(output);
+                });
+            }
+        }
+    }
+
+    public interface ITestWidget
+    {
+        string Foo();
+    }
+}
