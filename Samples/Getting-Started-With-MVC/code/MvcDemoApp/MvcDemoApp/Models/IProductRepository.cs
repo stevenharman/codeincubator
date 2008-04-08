d0f63a5703d68f5b1bc55244a3a70abc8fc26f4f
tim.wingfield <tim.wingfield@b7d3163e-863a-0410-ab12-b38f340628c1>
2008-04-08 23:28:20 +0000

    Refactored data context. Changed to repository. Started Product Controller Test.

---
+++ Samples/Getting-Started-With-MVC/code/MvcDemoApp/MvcDemoApp/Models/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvcDemoApp.Models
+{
+    public interface IProductRepository
+    {
+        List<Product> GetTenProducts();
+        Product GetProductById(int id);
+    }
+}
---
+++ Samples/Getting-Started-With-MVC/code/MvcDemoApp/MvcDemoApp/Models/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace MvcDemoApp.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+        private AdventureWorksDataContext db = new AdventureWorksDataContext();
+
+        public List<Product> GetTenProducts()
+        {
+            return db.Products.Where(p => p.ListPrice > 0.0M).Take(10).ToList<Product>();
+        }
+
+        public Product GetProductById(int id)
+        {
+            return db.Products.Single<Product>(p => p.ProductID == id);
+        }
+    }
+}
