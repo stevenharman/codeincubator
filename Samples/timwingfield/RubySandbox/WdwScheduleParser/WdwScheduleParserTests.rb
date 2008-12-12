dcef37f8a54b1ea88b61e399e0d8e9ab53eb87b3
Tim Wingfield <timwingfield@users.noreply.github.com>
2008-12-12 05:51:33 +0000

    Adding my ruby sandbox. Let the learning begin!

---
+++ Samples/timwingfield/RubySandbox/WdwScheduleParser/WdwScheduleParserTests.rb
@@ -0,0 +1,23 @@
+require 'test/unit'
+require "WdwScheduleParser"
+class WdwScheduleParserTest < Test::Unit::TestCase
+	
+	@@f = File.new("wdw_mk_march09.htm")
+	
+	def test_file_is_loaded
+		assert_not_nil(@@f)
+	end
+	
+	def test_file_is_readable
+		assert(File.readable?("wdw_mk_march09.htm"))
+	end
+	
+	def test_wdwScheduleParser_is_not_nill
+		assert_not_nil(WdwScheduleParser.new)
+	end
+	
+	def test_simple_output
+		wdw = WdwScheduleParser.new
+		assert_equal(wdw.simpleOutput, "wdw")
+	end
+end
