df3c133d5086977c8b27b66b21e7a19533003644
tim.wingfield <tim.wingfield@b7d3163e-863a-0410-ab12-b38f340628c1>
2008-12-21 02:06:46 +0000

    hours tests finished. date tests finished.

--- Samples/timwingfield/RubySandbox/WdwScheduleParser/WdwScheduleParser.rb
+++ Samples/timwingfield/RubySandbox/WdwScheduleParser/WdwScheduleParser.rb
@@ -1,5 +1,30 @@
 class WdwScheduleParser
-	def simpleOutput
-		"wdw"
+	def get_reg_hours(line)
+		cleaned = line.chomp.strip
+		start = cleaned.index('>') + 1
+		cleaned[start, cleaned.length]
+	end
+	
+	def is_emh_line?(line)
+		line.include? "<br>"
+	end
+	
+	def get_hours_from_emh_line(line)
+		start = line.index('>') + 1
+		stop = line.index("<br>")
+		line[start, stop]
+	end
+	
+	def line_contains_hours?(line)
+		line.include? "id=\"calendarText\""
+	end
+	
+	def line_is_the_date_line?(line)
+		line.include? "calendarOn"
+	end
+	
+	def get_date_from_date_line(line)
+		start = line.index("date=") + 1
+		line[start, 10]
 	end
 end
--- Samples/timwingfield/RubySandbox/WdwScheduleParser/WdwScheduleParserTests.rb
+++ Samples/timwingfield/RubySandbox/WdwScheduleParser/WdwScheduleParserTests.rb
@@ -2,22 +2,53 @@ require 'test/unit'
 require "WdwScheduleParser"
 class WdwScheduleParserTest < Test::Unit::TestCase
 	
-	@@f = File.open("wdw_mk_march09.htm")
+	@@a = IO.readlines("wdw_mk_march09.htm")
 	
-	def test_file_is_loaded
-		assert_not_nil(@@f)
-	end
+	@@emhLine = @@a[768]
+	@@regLine = @@a[773]
+	@@dateLine = @@a[767]
+	@@hours = "9:00am - 8:00pm"
+	@@date = "03/01/2009"
+	@@wdw = WdwScheduleParser.new
 	
 	def test_file_is_readable
 		assert(File.readable?("wdw_mk_march09.htm"))
 	end
 	
-	def test_wdwScheduleParser_is_not_nill
-		assert_not_nil(WdwScheduleParser.new)
+	def test_get_hours_from_regular_line
+		assert(@@wdw.get_reg_hours(@@regLine), @@hours)
 	end
 	
-	def test_simple_output
-		wdw = WdwScheduleParser.new
-		assert_equal(wdw.simpleOutput, "wdw")
+	def test_is_emh_line
+		assert(@@wdw.is_emh_line?(@@emhLine))
 	end
+	
+	def test_is_not_emh_line
+		assert(!@@wdw.is_emh_line?(@@regLine))
+	end
+	
+	def test_get_hours_from_emh_line
+		assert(@@wdw.get_hours_from_emh_line(@@emhLine), @@hours)
+	end
+	
+	def test_line_contains_hours
+		assert(@@wdw.line_contains_hours?(@@emhLine))
+	end
+	
+	def test_line_does_not_contain_hours
+		assert(!@@wdw.line_contains_hours?(@@a[770]))
+	end
+	
+	def test_line_is_the_date_line
+		assert(@@wdw.line_is_the_date_line?(@@dateLine))
+	end
+	
+	def test_line_is_not_the_date_line
+		assert(!@@wdw.line_is_the_date_line?(@@emhLine))
+	end
+	
+	def test_get_date_from_date_line
+		assert(@@wdw.get_date_from_date_line(@@dateLine))
+	end
+	
 end
